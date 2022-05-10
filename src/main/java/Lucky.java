c88fb59d943413c5040e00464b3eae16e0ba9a2c
Denis <Barkas074@yandex.ru>
2022-05-11 01:06:53 +0500

    Правки

--- src/main/java/Lucky.java
+++ src/main/java/Lucky.java
@@ -1,24 +1,25 @@
 public class Lucky {
     static int x = 0;
     static int count = 0;
-    private static Object object1 = new Object();
-    private static Object object2 = new Object();
+    private static final Object object1 = new Object();
+    private static final Object object2 = new Object();
 
     static class LuckyThread extends Thread {
         @Override
         public void run() {
             int xInThread;
             while (x < 999999) {
-                synchronized (object1){
+                synchronized (object1) {
                     x++;
                     xInThread = x;
+                    if (x > 999999)
+                        break;
                 }
-                if (x > 999999)
-                    break;
+
                 if ((xInThread % 10) + (xInThread / 10) % 10 + (xInThread / 100) % 10 == (xInThread / 1000)
                         % 10 + (xInThread / 10000) % 10 + (xInThread / 100000) % 10) {
                     System.out.println(xInThread + " " + Thread.currentThread().getName());
-                    synchronized (object2){
+                    synchronized (object2) {
                         count++;
                     }
                 }
